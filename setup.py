ae0cdb195840bacd8fe22bca2f59db4952b78555
barney9545 <darshan.ambule.da@gmail.com>
2024-06-21 19:45:41 +0530

    notebooks n more

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup,find_packages
 
+#->list[str] this is used for readablity of code and specifies that output is string
+def get_requirements()->list[str]:
+
+    requirements = list[str] =[]
+
+    return requirements
+
 setup(
     name='sensor',
     version='0.0.1',
     author='darshan',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=get_requirements()
 )
